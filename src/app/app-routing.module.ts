c616568c865050b03221aefaa18caf83615b0fc0
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2019-09-01 01:22:12 +0200

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ListComponent} from './videos/list/list.component';
+import {PlaylistComponent} from './videos/playlist/playlist.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/videos', pathMatch: 'full'},
+  {path: 'videos', component: ListComponent},
+  {path: 'playlist', component: PlaylistComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './videos/list/list.component';
+import { PlaylistComponent } from './videos/playlist/playlist.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    PlaylistComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
