4232857165996c6151c2d893a6bb9e7b2ea5bac1
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2019-10-04 15:39:30 +0200

    fix for production

--- src/app/videos/video/video.component.ts
+++ src/app/videos/video/video.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} f
 import {Video} from '../video';
 import {Store} from '@ngrx/store';
 import {State} from '../../store/reducers';
+import {MatButton} from '@angular/material';
 
 @Component({
   selector: 'app-video',
@@ -12,7 +13,7 @@ export class VideoComponent implements OnInit {
   @Input() video: Video;
   @Input() buttonLabel: string;
   @Output() clickImage = new EventEmitter<void>();
-  @Output() clickButton = new EventEmitter<HTMLButtonElement>();
+  @Output() clickButton = new EventEmitter<MatButton>();
 
   @ViewChild('check', {static: false}) checkInput: ElementRef;
 
@@ -33,7 +34,7 @@ export class VideoComponent implements OnInit {
     this.clickImage.emit();
   }
 
-  onClickButton(button: HTMLButtonElement) {
+  onClickButton(button: MatButton) {
     this.clickButton.emit(button);
   }
 
